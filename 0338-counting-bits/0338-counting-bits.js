5e408b5d0151dc95a46e9af88be8550ba357ded3
Jisoo Ahn <70849122+An-jisu@users.noreply.github.com>
2023-12-15 11:47:24 +0900

    Time: 132 ms (7.01%), Space: 53.7 MB (15.90%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let arr = [];
+    for(let i = 0;i<=n;i++){
+        let bin = '';
+        let num = i;
+        //이진수로 바꾸기 (문자열 형태로)
+        while(num/2!==0){
+            bin=bin+String(num%2);
+            num=Math.floor(num/2);
+        }
+        //해당 문자열에서 1갯수 세서 arr에 넣기
+        arr.push(bin.split("").filter((a)=> a==='1').length);
+    }
+    return arr;
+};
